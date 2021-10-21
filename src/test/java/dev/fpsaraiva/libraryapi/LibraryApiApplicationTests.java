c9b35da55731ddef5a7cc85db75f39a7d1c99c5b
Fernando Saraiva <fpsaraiva@gmail.com>
2021-10-21 09:48:57 -0300

    setup inicial

---
+++ src/test/java/dev/fpsaraiva/libraryapi/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fpsaraiva.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
