53488c04f4cc6086f8a441a6ad76556d26378928
Fernando Saraiva <fpsaraiva@gmail.com>
2021-11-18 10:36:05 -0300

    testes método update

--- src/test/java/dev/fpsaraiva/libraryapi/service/BookServiceTest.java
+++ src/test/java/dev/fpsaraiva/libraryapi/service/BookServiceTest.java
@@ -121,6 +121,35 @@ public class BookServiceTest {
         Mockito.verify(repository, Mockito.never()).delete(book);
     }
 
+    @Test
+    @DisplayName("Deve atualizar um livro")
+    public void updateBookTest() {
+        Long id = 1l;
+        Book updatingBook = createValidBook();
+        updatingBook.setId(id);
+
+        Book updatedBook = createValidBook();
+        updatedBook.setId(id);
+        Mockito.when(repository.save(updatingBook)).thenReturn(updatedBook);
+
+        Book book = service.update(updatingBook);
+
+        assertThat(book.getId()).isEqualTo(updatedBook.getId());
+        assertThat(book.getTitle()).isEqualTo(updatedBook.getTitle());
+        assertThat(book.getAuthor()).isEqualTo(updatedBook.getAuthor());
+        assertThat(book.getIsbn()).isEqualTo(updatedBook.getIsbn());
+    }
+
+    @Test
+    @DisplayName("Deve ocorrer erro ao tentar atualizar um livro existente")
+    public void updateInvalidBookTest() {
+        Book book = new Book();
+
+        assertThrows(IllegalArgumentException.class, () -> service.update(book));
+
+        Mockito.verify(repository, Mockito.never()).save(book);
+    }
+
     private Book createValidBook() {
         return new Book( "As Aventuras", "Fulano", "1111");
     }
