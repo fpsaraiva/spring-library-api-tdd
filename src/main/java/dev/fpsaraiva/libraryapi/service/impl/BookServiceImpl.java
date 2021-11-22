80e76cd931125e060366a8fffe26c796cec9eddc
Fernando Saraiva <fpsaraiva@gmail.com>
2021-11-22 15:36:40 -0300

    implementando filtro de livros com Example do Spring data

--- src/main/java/dev/fpsaraiva/libraryapi/service/impl/BookServiceImpl.java
+++ src/main/java/dev/fpsaraiva/libraryapi/service/impl/BookServiceImpl.java
@@ -4,6 +4,8 @@ import dev.fpsaraiva.libraryapi.exception.BusinessException;
 import dev.fpsaraiva.libraryapi.model.Book;
 import dev.fpsaraiva.libraryapi.model.repository.BookRepository;
 import dev.fpsaraiva.libraryapi.service.BookService;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -50,6 +52,13 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public Page<Book> find(Book filter, Pageable pageRequest) {
-        return null;
+        Example<Book> example = Example.of(filter,
+                ExampleMatcher
+                        .matching()
+                        .withIgnoreCase()
+                        .withIgnoreNullValues()
+                        .withStringMatcher(ExampleMatcher.StringMatcher.STARTING)
+        );
+        return repository.findAll(example, pageRequest);
     }
 }
