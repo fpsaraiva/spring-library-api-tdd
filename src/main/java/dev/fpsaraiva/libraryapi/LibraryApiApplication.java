96e4785fbc3412997ab05d14e7820802c522a413
Fernando Saraiva <fpsaraiva@gmail.com>
2021-11-26 15:20:06 -0300

    implementando alterações para receber email customer

--- src/main/java/dev/fpsaraiva/libraryapi/LibraryApiApplication.java
+++ src/main/java/dev/fpsaraiva/libraryapi/LibraryApiApplication.java
@@ -2,8 +2,10 @@ package dev.fpsaraiva.libraryapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LibraryApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/dev/fpsaraiva/libraryapi/api/dto/LoanDTO.java
+++ src/main/java/dev/fpsaraiva/libraryapi/api/dto/LoanDTO.java
@@ -1,9 +1,17 @@
 package dev.fpsaraiva.libraryapi.api.dto;
 
+import javax.validation.constraints.NotBlank;
+
 public class LoanDTO {
 
+    @NotBlank
     private String isbn;
+
+    @NotBlank
     private String customer;
+
+    @NotBlank
+    private String email;
     private BookDTOResponse book;
 
     public LoanDTO() {
@@ -12,6 +20,12 @@ public class LoanDTO {
     public LoanDTO(String isbn, String customer) {
         this.isbn = isbn;
         this.customer = customer;
+    }
+
+    public LoanDTO(String isbn, String customer, String email) {
+        this.isbn = isbn;
+        this.customer = customer;
+        this.email = email;
     }
 
     //criado para LoanControllerTest
@@ -27,5 +41,9 @@ public class LoanDTO {
 
     public String getCustomer() {
         return customer;
+    }
+
+    public String getEmail() {
+        return email;
     }
 }
