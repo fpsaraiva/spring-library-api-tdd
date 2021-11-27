825afce8b1e1fe51d0151b58e33890adccbd38a4
Fernando Saraiva <fpsaraiva@gmail.com>
2021-11-27 17:02:51 -0300

    documentando a api com swagger

---
+++ src/main/java/dev/fpsaraiva/libraryapi/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package dev.fpsaraiva.libraryapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("dev.fpsaraiva.libraryapi.api.resource"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Library API")
+                .description("Prática API com TDD")
+                .version("1.0")
+                .contact(contact())
+                .build();
+    }
+
+    private Contact contact() {
+        return new Contact("Fernando Saraiva", "https://github.com/fpsaraiva", "fernando@fpsaraiva.dev");
+    }
+}
